c657ff96a869d5ba2598eb3254335474e9842bf4
Henok Mekonnen <henatan99@yahoo.com>
2023-08-31 19:46:01 -0400

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,18 +2,44 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
-
+  // Name
+  string public name = "DApp Token";
+  // Symbol
+  string public symbol = "DAPP";
+  // Standard 
+  string public standard = "DApp Token v1.0";
   // state variables
   uint256 public totalSupply;
 
+  event Transfer(
+    address _from, 
+    address _to,
+    uint256 _value
+  );
+  
+  mapping(address => uint256) public balanceOf;
+
   // constructor
 
-  constructor() public {
-    totalSupply = 1000000;
+  constructor(uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    // Set the total number of tokens
+    totalSupply = _initialSupply;
   }
 
-  // Set the total number of tokens
+  // Transfer
+  
 
-  // Read the total number of tokens
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    // Exception if account doesn't have enough
+    require(balanceOf[msg.sender] >= _value);
+    // Transfer the balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    // Transfer event 
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+  }
   
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(DappToken);
+  deployer.deploy(DappToken, 1000000);  // 1000000 is the argument passed in constructor function
 };
